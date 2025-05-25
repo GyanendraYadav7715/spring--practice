33666ab6ca70cde2cea36a00810047baf911c6cb
Gyan <gyanendrayadav12345789@gmail.com>
2025-05-25 18:43:51 +0530

    all done

--- src/main/java/com/codeaura/store/StoreApplication.java
+++ src/main/java/com/codeaura/store/StoreApplication.java
@@ -10,11 +10,7 @@ public class StoreApplication {
     public static void main(String[] args) {
 
         ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-        var orderService = context.getBean(OrderService.class);
-        orderService.placeOrder();
 
-        var notificationManager = context.getBean(NotificationManager.class);
-        notificationManager.sendingMessagesQueue("RAM RAM BHAYIOOO");
 
     }
 
